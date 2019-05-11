412f2c7fddb026baa462452f537716d382ff9051
wakazoku <wakazoku@gmail.com>
2019-05-12 00:30:14 +0900

    chapter2終了

--- src/chapter2/directive/main.js
+++ src/chapter2/directive/main.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: "#app",
   data: {
     message: "Vue.jsの公式HPですよ",
-    url: "https://jp.vuejs.org/index.html"
+    url: "https://jp.vuejs.org/index.html",
+    tag: "Hello <strong>Vue.js!!</strong>"
   },
   created: function() {},
   mounted: function() {},
