4e31bc75b78b70ba90c252922fadf81c0e4c5276
wakazoku <wakazoku@gmail.com>
2019-05-17 01:28:22 +0900

    フォーム入力バインディング（テキスト、チェックボックス、ラジオボックス）

---
+++ src/chapter3/form_binding/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Vue.jsの公式HPですよ",
+    checkbox: [],
+    radio: ""
+  },
+  created: function() {},
+  mounted: function() {},
+  methods: {}
+});
