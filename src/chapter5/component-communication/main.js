33ff8c0309ae1cb119dc7e2dba1c1dbad4ece5ba
Wakatabi <t_wakatabi@uluru.jp>
2019-07-16 21:13:23 +0900

    子コンポーネントから親コンポーネントへの通信

--- src/chapter5/component-communication/main.js
+++ src/chapter5/component-communication/main.js
@@ -96,18 +96,76 @@
 // ----------------------
 // -子から親へ
 // ----------------------
-Vue.component("comp-child", {
-  template: "<p>{{ val }}</p>",
-  props: {
-    val: Number
+// Vue.component("comp-child", {
+//   template: "<button v-on:click='handleClick'>イベント発火</button>",
+//   methods: {
+//     // ボタンのクリックイベントのハンドラでchilds-eventを発火させる
+//     handleClick: function() {
+//       this.$emit("childs-event");
+//     }
+//   }
+// });
+
+// new Vue({
+//   el: "#app",
+//   methods: {
+//     // childs-eventsが発生
+//     parentMethod: function(){
+//       alert('イベントキャッチ')
+//     }
+
+//   },
+//   data: {}
+// });
+
+// ----------------------
+// -親が持つデータお操作しよう
+// ----------------------
+// Vue.component("comp-child", {
+//   template:
+//     "<li> {{ name }} HP. {{ hp }} <button v-on:click='doAttack'>攻撃する</button></li> ",
+//   props: { id: Number, name: String, hp: Number },
+//   methods: {
+//     // ボタンのクリックイベントのハンドラでchilds-eventを発火させる
+//     doAttack: function() {
+//       // 引数として自分のIDを渡す
+//       this.$emit("attack", this.id);
+//     }
+//   }
+// });
+
+// new Vue({
+//   el: "#app",
+//   data: {
+//     list: [
+//       { id: 1, name: "スライム", hp: 100 },
+//       { id: 2, name: "ゴブリン", hp: 200 },
+//       { id: 3, name: "ドラゴン", hp: 500 }
+//     ]
+//   },
+//   methods: {
+//     // attackが発生
+//     handleAttack: function(id) {
+//       // 引数のIDから要素を検索
+//       var item = this.list.find(function(el) {
+//         return el.id === id;
+//       });
+//       // HPが0より大きければ10減らす
+//       if (item !== undefined && item.hp > 0) item.hp -= 10;
+//     }
+//   }
+// });
+
+// ----------------------
+// - 非親子コンポーネント
+// ----------------------
+var bus = new Vue({
+  data: {
+    count: 0
   }
 });
-Vue.component("example", {
-  template: "",
-  props: {}
-});
-
-new Vue({
-  el: "#app",
-  data: {}
+bus.$emit("bus-event");
+bus.$on("bus-event", function() {
+  // busのイベントが起きたときにする処理
+  // 無名関数内のthisはbusのインスタンス
 });
